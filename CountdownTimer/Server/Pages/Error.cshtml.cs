28bf2c1bc6c51b05742473cc24d105acdf28504b
Artur Lipin <cookiess718@gmail.com>
2022-12-05 00:40:42 +0200

    timer section update, webassembly up

--- CountdownTimer/Server/Pages/Error.cshtml.cs
+++ CountdownTimer/Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace CountdownTimer.Pages
+namespace CountdownTimer.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
